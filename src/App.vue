17644ce91fdaeea26ff4bc528d97f831662b19a8
PingShiGuo <pingshiguo@gmail.com>
2018-03-14 00:06:34 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,34 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <button type="button" class="btn" @click="showModal">打开Modal</button>
+    <modal v-show="isModalVisible" @close="closeModal"></modal>
   </div>
 </template>
 
 <script>
+import Modal from '@/components/Modal'
+
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    Modal
+  },
+  data () {
+    return {
+      isModalVisible: false
+    }
+  },
+  methods: {
+    showModal: function () {
+      this.isModalVisible = true
+    },
+    closeModal: function () {
+      this.isModalVisible = false
+    }
+  }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Modal from '@/components/Modal'
 
 Vue.use(Router)
 
@@ -8,8 +8,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Modal',
+      component: Modal
     }
   ]
 })
